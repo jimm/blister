63ef04066aa7b8d1e2da1350233e5bcb595e58d2
Jim Menard <jim.menard@chloeandisabel.com>
2016-07-20 15:57:54 -0400

    fixed command line arg handling and list

--- lib/blister.ex
+++ lib/blister.ex
@@ -3,29 +3,35 @@ defmodule Blister do
   require Logger
 
   def start(_type, _args) do
-    argv = System.argv
-    if length(argv) > 0 && hd(argv) == "list" do
-      list_devices
-      {:ok, self}
-    else
-      run
-    end
+    run(System.argv)
   end
 
-  defp list_devices do
+  defp run(["list"]) do
     %{input: inputs, output: outputs} = PortMidi.devices
     f = fn d -> IO.puts "  #{d.name}" end
     IO.puts "Inputs:"
     inputs |> Enum.map(f)
     IO.puts "Outputs:"
     outputs |> Enum.map(f)
+    :init.stop
+    {:ok, self}
+  end
+  defp run(["text"]) do
+    do_run(Blister.GUI.Text)
+  end
+  defp run(["text", commands]) do
+    do_run(Blister.GUI.Text, fn -> Blister.GUI.Text.set_commands(commands |> to_char_list) end)
+  end
+  defp run([]) do
+    do_run(Blister.GIU.Curses)
   end
 
-  defp run do
+  defp do_run(gui_module, gui_config_func \\ nil) do
     Logger.info("starting supervisor")
-    # TODO make gui_module selectable
-    result = Blister.Supervisor.start_link(Blister.GUI.Text)
-    Blister.GUI.Text.set_commands('hjknpq')
+    result = Blister.Supervisor.start_link(gui_module)
+    if gui_config_func do
+      gui_config_func.()
+    end
     Logger.debug("supervisor started result = #{inspect result}, calling start_command_loop")
     Blister.Controller.start_command_loop
     receive do
