1f93b11c2c23ec2d9de70a6b24c9ffd5c0c3b9f5
Jim Menard <jim.menard@chloeandisabel.com>
2016-10-20 15:38:39 -0400

    local test setup

---
+++ examples/pipe_to_simple.exs
@@ -0,0 +1,55 @@
+%{inputs: [
+    {"IAC Driver Bus 1", :mac, "MacOS"},
+    {"MidiPipe Output 1", :mp, "MidiPipe"}
+  ],
+  outputs: [
+    {"IAC Driver Bus 1", :mac, "MacOS"},
+    {"MidiPipe Input 1", :mp, "MidiPipe"},
+    {"SimpleSynth virtual input", :ss, "SimpleSynth"},
+  ],
+  messages: [
+    {"Tune Request", {C.tune_request}},
+    {"Full Volume", C.midi_channels
+      |> Enum.map(fn chan -> {C.controller(chan), C.cc_volume, 127} end)
+      |> List.flatten}
+  ],
+  message_keys: %{f1: "Tune Request",
+                  f2: "Full Volume"},
+  triggers: [
+    # All on MIDI channel 1
+    {:mb, {C.controller, C.cc_gen_purpose_5, 127}, &Pack.next_patch/0},
+    {:mb, {C.controller, C.cc_gen_purpose_6, 127}, &Pack.prev_patch/0},
+    {:mb, {C.controller, C.cc_gen_purpose_7, 127}, &Pack.next_song/0},
+    {:mb, {C.controller, C.cc_gen_purpose_8, 127}, &Pack.prev_song/0},
+    {:mb, {C.controller, 126, 127}, fn -> Pack.send_message("Tune Request") end}
+  ],
+  songs: [
+    %{name: "First Song",
+      notes: """
+      Notes about this song
+      can span multiple lines.
+      """,
+      patches: [
+        %{name: "First Song, First Patch",
+          start_bytes: [C.tune_request],
+          connections: [
+            %{io: {:mp, :ss, 1},
+              prog_chg: 64, zone: (64..75), xpose: 12},
+          ]},
+        %{name: "First Song, Second Patch"}
+      ]},
+
+    %{name: "Second Song",
+      patches: [
+        %{name: "Second Song, First Patch",
+          connections: [
+            %{io: {:mp, :ss, 1},
+              prog_chg: 64, zone: (64..75), xpose: 12},
+          ]}]},
+    %{name: "Second Song, Second Patch"}],
+  song_lists: [
+    %{name: "Tonight's Song List",
+      songs: [
+        "First Song",
+        "Second Song"
+      ]}]}
